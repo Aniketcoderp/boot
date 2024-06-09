ded14fe1bd0ef58fb97f3205de8905a9d73d6ec6
Aniketcoderp <aniketsavita18@gmail.com>
2024-06-09 12:31:19 +0530

    add

---
+++ pai.py
@@ -0,0 +1,24 @@
+def marging(a,b,n):
+    i=0
+    j=0
+    r=[]
+    while i<n and j<n:
+        if a[i]<=b[j]:
+            r.append(a[i])
+            i+=1
+        else:
+            r.append(b[j])
+            j+=1
+    while i<n:
+         r.append(a[i])
+         i+=1
+    while j<n:
+         r.append(b[j])
+         j+=1
+    return r
+
+n=4
+a=[1,5,7,9]
+b=[2,4,6,8]
+ans=marging(a,b,n)
+print(ans)
