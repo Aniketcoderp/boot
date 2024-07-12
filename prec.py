93d778e418efe99a82e75d929f83a64f0fb4b08a
Aniketcoderp <aniketsavita18@gmail.com>
2024-07-12 23:17:04 +0530

    any

--- prec.py
+++ prec.py
@@ -1,13 +1,25 @@
-def somosa(n,k,shops):
-    shops.sort()
-    s=0
-    count=0
-    for i in range(n):
-        if s>=k:
-            break
-        s+=shops[i]
-        count+=1
-    return count
-n,k=4,10
-shops=[5,4,2,4]
-print(somosa(n,k,shops))
+def marge(a,b,n):
+    i=0
+    j=0
+    r=[]
+    while i<n and j<n:
+        if a[i]<=b[j]:
+            r.append(a[i])
+            i+=1
+        else:
+            r.append(b[j])
+            j+=1
+    while i<n:
+        r.append(a[i])
+        i+=1
+    while j<n:
+        r.append(b[j])
+        j+=1
+    print(*r)
+
+n=4
+a=[1,5,7,9]
+b=[2,4,6,8]
+marge(a,b,n)
+
+         
